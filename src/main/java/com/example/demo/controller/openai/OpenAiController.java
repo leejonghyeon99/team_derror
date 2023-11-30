e0ac17933f19e2bcb33ba515535be1e4349f7a69
leejonghyeon <rmaskdkfhk@gmali.com>
2023-11-30 16:31:32 +0900

    gitignore update

--- src/main/java/com/example/demo/controller/openai/OpenAiController.java
+++ src/main/java/com/example/demo/controller/openai/OpenAiController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller.openai;
 
+import com.example.demo.service.openai.GptService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/openai")
 public class OpenAiController {
 
+    @Autowired
+    GptService service;
 
     @GetMapping("/img")
     public ResponseEntity<String> test(){
-
+        service.test3();
         return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
--- src/main/java/com/example/demo/service/openai/GptService.java
+++ src/main/java/com/example/demo/service/openai/GptService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service.openai;
 
 import com.theokanning.openai.assistants.AssistantRequest;
+import com.theokanning.openai.assistants.AssistantToolsEnum;
+import com.theokanning.openai.assistants.Tool;
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.completion.CompletionResult;
 import com.theokanning.openai.completion.chat.*;
@@ -65,11 +67,29 @@ public class GptService {
 
    public void test3(){
        OpenAiService service = new OpenAiService(token);
-
+       String instructions = "입력받은 키워드로 명소 추천" +
+               "최대 5개 명사만 출력" +
+               "명사로만 출력하고 경도, 위도 같이 출력" +
+               "출력형식" +
+               "{" +
+               "  \"recommendations\": [" +
+               "    {" +
+               "      \"place\": String," +
+               "      \"longitude\": String," +
+               "      \"latitude\": String" +
+               "    }," +
+               "  ]" +
+               "}";
+       List<Tool> tools = new ArrayList<>();
+       Tool tool = new Tool();
+       tool.setType(AssistantToolsEnum.CODE_INTERPRETER);
+       tools.add(tool);
        AssistantRequest assistantRequest = AssistantRequest
                .builder()
+               .name("recommendation")
                .model("gpt-3.5-turbo-1106")
                .instructions("입력받은 문자열을 연상되는 단어만 답해줘")
+               .tools(tools)
                .build();
 
    }
