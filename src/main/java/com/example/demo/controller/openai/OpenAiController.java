a996287a6dbdd56c275d032abe47c06b87c54ad3
leejonghyeon <rmaskdkfhk@gmali.com>
2023-12-03 18:45:22 +0900

    openai domain add

--- src/main/java/com/example/demo/controller/openai/OpenAiController.java
+++ src/main/java/com/example/demo/controller/openai/OpenAiController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller.openai;
 
+import com.example.demo.domain.openai.Sight;
 import com.example.demo.service.openai.GptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,10 +20,9 @@ public class OpenAiController {
     @Autowired
     GptService service;
 
-    @GetMapping("/img")
-    public ResponseEntity<String> test(){
-        service.test3();
-        return new ResponseEntity<>(null, HttpStatus.OK);
+    @GetMapping("/recommendation")
+    public ResponseEntity<Sight> search(@RequestParam String keyword){
+        return new ResponseEntity<>(service.searchKeyWord(keyword), HttpStatus.OK);
     }
 
 
--- src/main/java/com/example/demo/service/openai/GptService.java
+++ src/main/java/com/example/demo/service/openai/GptService.java
@@ -1,9 +1,13 @@
 package com.example.demo.service.openai;
 
+import com.example.demo.domain.openai.Sight;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.theokanning.openai.OpenAiResponse;
 import com.theokanning.openai.completion.chat.*;
 import com.theokanning.openai.image.CreateImageRequest;
 import com.theokanning.openai.messages.Message;
+import com.theokanning.openai.messages.MessageContent;
 import com.theokanning.openai.messages.MessageRequest;
 import com.theokanning.openai.messages.content.Text;
 import com.theokanning.openai.runs.Run;
@@ -30,44 +34,7 @@ public class GptService {
     @Value(value = "${openai.key}")
     private String token;
 
-   public void test(){
-
-       OpenAiService service = new OpenAiService(token, Duration.ofSeconds(30));
-
-       System.out.println("\nCreating Image...");
-       CreateImageRequest request = CreateImageRequest.builder()
-               .model("dall-e-3")
-               .quality("hd")
-               .prompt("오락실에서 게임하는 거북이")
-               .build();
-
-       System.out.println("\nImage is located at:");
-       System.out.println(service.createImage(request).getData().get(0).getUrl());
-   }
-
-   public void test2(){
-       OpenAiService service = new OpenAiService(token, Duration.ofSeconds(30));
-       System.out.println("GPT 연결");
-       final List<ChatMessage> messages = new ArrayList<>();
-       final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), "오늘의 날씨 정보 알려줘");
-       messages.add(systemMessage);
-       ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
-               .builder()
-               .model("gpt-3.5-turbo")
-               .messages(messages)
-               .n(1)
-               .maxTokens(50)
-               .logitBias(new HashMap<>())
-               .build();
-       service.createChatCompletion(chatCompletionRequest)
-               .getChoices().forEach(System.out::println);
-
-       //OpenAi 연결 종료
-       service.shutdownExecutor();
-       System.out.println("GPT 종료");
-   }
-
-   public void test3(){
+   public Sight searchKeyWord(String keyword){
        OpenAiService service = new OpenAiService(token);
         String id = "asst_Es5Gqn9qr4aonTRGvLAgS9uE";
 
@@ -77,7 +44,7 @@ public class GptService {
        Thread thread = service.createThread(threadRequest);
 
        MessageRequest messageRequest = MessageRequest.builder()
-               .content("스위스 강가 절벽")
+               .content(keyword)
                .build();
        service.createMessage(thread.getId(),messageRequest);
 
@@ -93,7 +60,14 @@ public class GptService {
        while (!(retrievedRun.getStatus().equals("completed")) && !(retrievedRun.getStatus().equals("failed")));
 
        OpenAiResponse<Message> messageOpenAiResponse = service.listMessages(thread.getId());
+       ObjectMapper mapper = new ObjectMapper();
+       Sight sight = null;
+       try {
+           sight = mapper.readValue(messageOpenAiResponse.getData().get(0).getContent().get(0).getText().getValue(), Sight.class);
+       } catch (JsonProcessingException e) {
+           throw new RuntimeException(e);
+       }
 
-       System.out.println(messageOpenAiResponse.getData().get(0).getContent());
+       return sight;
    }
 }
