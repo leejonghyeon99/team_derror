3a92e6d552762fe8011f458db39263250748436a
leejonghyeon <rmaskdkfhk@gmali.com>
2023-12-06 12:35:46 +0900

    main page add

--- ERD/derror_dml.sql
+++ ERD/derror_dml.sql
@@ -1,5 +1,16 @@
-INSERT INTO authority (name) VALUES ('USER'), ('ADMIN');
+INSERT INTO authority (id,name) VALUES (1,'USER'), (2,'ADMIN');
 select * from authority;
 select * from member;
+
+
+Insert into member (auth_id,username,password,name,age,phone, email)
+values ('1','testid', 'testpw','testname',23,'010-1234-1234','testemail@email.com');
+
+select * from post;
+
+insert into post (member_id, category, title, content)
+values (2,'testcategory','testtitle','testcontent');
+
+
 
 
