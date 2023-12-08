6cd9ede0f969f5a3fe9fc06e68b46140a8b6f341
Langssa <imsen456@gmail.com>
2023-12-08 10:10:01 +0900

    test

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig {
                 .build();
 
     }
+    //manager
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
         return authenticationConfiguration.getAuthenticationManager();
