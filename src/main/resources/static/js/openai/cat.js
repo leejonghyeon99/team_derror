f499aee328a36d0236cc97359d8ca494b1e45cb8
leejonghyeon <rmaskdkfhk@gmali.com>
2023-12-08 16:34:43 +0900

    고양이 키보드 2개에서 4개로 추가

--- src/main/resources/static/js/openai/cat.js
+++ src/main/resources/static/js/openai/cat.js
@@ -1,13 +1,19 @@
 const box = document.getElementById("keyword");
-const leftUp = document.getElementById("left-up");
-const leftDown = document.getElementById("left-down");
-const rightUp = document.getElementById("right-up");
-const rightDown = document.getElementById("right-down");
+const leftDown = document.getElementsByClassName("left-down");
+const leftUp = document.getElementsByClassName("left-up");
+const rightDown = document.getElementsByClassName("right-down");
+const rightUp = document.getElementsByClassName("right-up");
 
-leftUp.style.display = 'none';
-leftDown.style.display = 'none';
-rightUp.style.display = 'none';
-rightDown.style.display = 'none';
+disNone();
+function disNone(){
+    leftUp[0].style.display = 'none';
+    leftDown[0].style.display = 'none';
+    leftDown[1].style.display = 'none';
+
+    rightUp[0].style.display = 'none';
+    rightDown[0].style.display = 'none';
+    rightDown[1].style.display = 'none';
+}
 
 const leftKeys = ['KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'Space'];
 const rightKeys = ['KeyU', 'KeyJ', 'KeyN', 'KeyI', 'KeyK', 'KeyM', 'KeyO', 'KeyL', 'KeyP', 'BracketLeft', 'BracketRight', 'Semicolon', 'Quote', 'Comma', 'Period', 'Slash', 'Enter'];
@@ -19,37 +25,37 @@ function isInput(){
     }else{
         bool = true;
     }
-    return true;
+    return bool;
 }
-
+function setRnd(){
+    let rnd = Math.round(Math.random());
+    return rnd;
+}
+let rnd;
 box.addEventListener("keydown", function(event){
+    rnd = setRnd();
     if(isInput()){
-        leftUp.style.display = 'none';
-        leftDown.style.display = 'none';
-        rightUp.style.display = 'none';
-        rightDown.style.display = 'none';
+        disNone();
     }
     if (leftKeys.includes(event.code)) {
-        leftUp.style.display = 'block';
-        leftDown.style.display = 'none';
+        leftUp[0].style.display = 'none';
+        leftDown[rnd].style.display = 'block';
     }else if(rightKeys.includes(event.code)){
-        rightUp.style.display = 'block';
-        rightDown.style.display = 'none';
+        rightUp[0].style.display = 'none';
+        rightDown[rnd].style.display = 'block';
     }
 })
 
 box.addEventListener("keyup", function(event){
+    rnd = setRnd();
     if(isInput()){
-        leftUp.style.display = 'none';
-        leftDown.style.display = 'none';
-        rightUp.style.display = 'none';
-        rightDown.style.display = 'none';
+        disNone();
     }
     if (leftKeys.includes(event.code)) {
-        leftUp.style.display = 'none';
-        leftDown.style.display = 'block';
+        leftUp[0].style.display = 'block';
+        leftDown[rnd].style.display = 'none';
     }else if(rightKeys.includes(event.code)){
-        rightUp.style.display = 'none';
-        rightDown.style.display = 'block';
+        rightUp[0].style.display = 'block';
+        rightDown[rnd].style.display = 'none';
     }
 })
