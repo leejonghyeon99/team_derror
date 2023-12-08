79dad75bbea528ed463a30cb0c597965b55edc11
GOUN <goun3596@gmail.com>
2023-12-08 09:35:05 +0900

    12.08 권한 수정중

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -16,6 +16,12 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        // URL 과 접근권한 세팅(들)
+                        // ↓ /board/detail/** URL로 들어오는 요청은 '인증'만 필요.
+                        .requestMatchers("/board/detail/**").authenticated()
+                        // ↓ "/board/write/**", "/board/update/**", "/board/delete/**" URL로 들어오는 요청은 '인증' 뿐 아니라 ROLE_MEMBER 나 ROLE_ADMIN 권한을 갖고 있어야 한다. ('인가')
+                        .requestMatchers("/board/write/**", "/board/update/**", "/board/delete/**").hasAnyRole("USER", "ADMIN")
+                        // ↓ 그 밖의 다른 요청은 모두 permit!
                         .anyRequest().permitAll()
                 )
                 .formLogin(form -> form
