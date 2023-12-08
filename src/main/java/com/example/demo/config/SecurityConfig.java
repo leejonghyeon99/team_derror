d4a9da0107adeb010dc906af0d0db2196db399ed
GOUN <goun3596@gmail.com>
2023-12-08 10:17:07 +0900

    pull

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -54,7 +54,6 @@ public class SecurityConfig {
                 .build();
 
     }
-    //manager
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
         return authenticationConfiguration.getAuthenticationManager();
