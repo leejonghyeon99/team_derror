5ff05cc70a1bf07c5824ea2160d51ac350dfcfbb
leejonghyeon <rmaskdkfhk@gmali.com>
2023-12-28 09:22:37 +0900

    프로젝트 배포

--- src/main/java/com/example/demo/DerrorDemoApplication.java
+++ src/main/java/com/example/demo/DerrorDemoApplication.java
@@ -2,12 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DerrorDemoApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DerrorDemoApplication.class, args);
 	}
 
 }
