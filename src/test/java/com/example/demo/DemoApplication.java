000cc35bdadf9bd857b12381c73edd48f9f72733
leejonghyeon <rmaskdkfhk@gmali.com>
2023-12-28 13:04:31 +0900

    프로젝트 배포3

--- src/test/java/com/example/demo/DemoApplication.java
+++ src/test/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,11 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplication {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class DemoApplication {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
